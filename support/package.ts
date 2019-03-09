756bbf244bff057f60ea3ba1bbdb100dc7c22011
Ben <codeandcats@gmail.com>
2019-03-09 22:21:39 +1100

    ci(package): update package version before shipping

--- support/package.ts
+++ support/package.ts
@@ -1,25 +1,23 @@
 import * as fs from 'fs';
+import { rootPath } from 'get-root-path';
 import * as path from 'path';
-import resolveAppPath = require('resolve-app-path');
-
-const appPath = resolveAppPath();
 
 fs.copyFileSync(
-  path.join(appPath, './package.json'),
-  path.join(appPath, './dist/package.json')
+  path.join(rootPath, './package.json'),
+  path.join(rootPath, './dist/package.json')
 );
 
 fs.copyFileSync(
-  path.join(appPath, './README.md'),
-  path.join(appPath, './dist/README.md')
+  path.join(rootPath, './README.md'),
+  path.join(rootPath, './dist/README.md')
 );
 
 fs.copyFileSync(
-  path.join(appPath, './logo.png'),
-  path.join(appPath, './dist/logo.png')
+  path.join(rootPath, './logo.png'),
+  path.join(rootPath, './dist/logo.png')
 );
 
-const packageJson = fs.readFileSync(path.join(appPath, './dist/package.json'), 'utf8');
+const packageJson = fs.readFileSync(path.join(rootPath, './dist/package.json'), 'utf8');
 const packageObject = JSON.parse(packageJson);
 
 delete packageObject.private;
@@ -28,7 +26,7 @@ delete packageObject.devDependencies;
 delete packageObject.husky;
 
 fs.writeFileSync(
-  path.join(appPath, './dist/package.json'),
+  path.join(rootPath, './dist/package.json'),
   JSON.stringify(packageObject, null, '  '),
   'utf8'
 );
