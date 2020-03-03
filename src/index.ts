2b98a5363031e067db1c379c43604d8804b8cb02
Ben <codeandcats@gmail.com>
2020-03-03 13:11:56 +1100

    build(deps-dev): install latest dependencies

--- src/index.ts
+++ src/index.ts
@@ -255,8 +255,8 @@ export function deepPartialOf<TObjectToMock>(partial: DeepPartial<TObjectToMock>
 }
 
 export type DeepPartial<T> = {
-  [P in keyof T]?: T[P] extends Array<infer U>
-  ? Array<DeepPartial<U>>
+  [P in keyof T]?: T[P] extends (infer U)[]
+  ? DeepPartial<U>[]
   : T[P] extends ReadonlyArray<infer U2>
   ? ReadonlyArray<DeepPartial<U2>>
   : DeepPartial<T[P]>
